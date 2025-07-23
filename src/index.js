6d94ac5ffea981444db2f9a6fed6e7271e4e2f88
vansh <vanshgularia@gmail.com>
2025-07-23 10:24:01 +0530

    userAndVideoModel

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 // require('dotenv').config({path: '../.env'});
 import connectDB from "./db/index.js";
-import dotenv from "dotenv"
-
-dotenv.config({path:'../.env'})
-connectDB()
-
-
-/*
+import dotenv from "dotenv";
 import express from "express"
 const app=express()
+ 
+dotenv.config({
+  path: "../.env",
+});
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("Error in Express server:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+    process.exit(1);
+  });
+
+/*
 
 ;(async()=>{
     try {
---
+++ src/models/users.model.js
@@ -0,0 +1,94 @@
+import mongoose,{Schema} from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+const userSchema=new Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true,
+            index:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true
+        },
+         fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar:{
+            type:String,
+            required:true,
+            unique:true,
+            lowercase:true,
+            trim:true
+        },
+        coverImage:{
+            type:String
+        },
+        watchHistory:[
+            {
+                type:Schema.Types.ObjectId,
+                ref:"Video"
+            }
+        ],
+        password:{
+            type:String,
+            required: [true,'password is required']
+        },
+        refrenceToken:{
+            type:String
+        }
+
+},
+{
+    timestamps:true
+}
+)
+
+userSchema.pre("save",async function (next) {
+    if(!this.isModified("password"))next();
+
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password,this.password)
+}
+userSchema.method.genrateAccessToken = function(){
+   return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expireIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.method.genrateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expireIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const user =mongoose.model("User",userSchema)
